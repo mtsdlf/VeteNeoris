13c6499824b26abb328abf18937623db37c1d95c
matiasdelafuente <matiasdelafuente@RISARO-PC.cronos.com.ar>
2019-08-28 15:43:28 -0300

    v1

---
+++ NeoVet/src/modelo/empleado/Veterinario.java
@@ -0,0 +1,10 @@
+package modelo.empleado;
+
+import modelo.empleados;
+
+public class Veterinario extends empleados{
+
+	public Veterinario(String nombre,String apellido,int edad,int idLegajo) {
+		super(nombre, apellido, edad, idLegajo);
+	}
+}
